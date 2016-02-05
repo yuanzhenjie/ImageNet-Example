543636f3aa2c30b905438f58b98562021c9d2c1d
nyghtowl <warrick.melanie@gmail.com>
2016-02-04 19:06:32 -0800

    Reworking how to load and group data to scale for larger datasets. Setting up grouping into large transformed, sequence files with diverse examples.

---
+++ src/main/java/imagenet/Utils/DataUtils.java
@@ -0,0 +1,168 @@
+package imagenet.Utils;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+
+/**
+ * Utilities to load, parse, relabel data.
+ * Target to pull in mixed batches of images and store as sequence files
+ * Leverage PreProcess data to store
+ */
+public class DataUtils {
+    private static final Logger log = LoggerFactory.getLogger(DataUtils.class);
+
+    // TODO pass in the main paths
+    protected static final String TEMP_DIR = FilenameUtils.concat(System.getProperty("java.io.tmpdir"), "ImageNetSeqFiles");
+    protected static String basePath = ImageNetLoader.BASE_DIR;
+    protected static String inputFolder = "train";
+    protected static String outputFolder = "seqTrain";
+    protected static String inputPath = FilenameUtils.concat(basePath, inputFolder);
+    protected static String outputPath = FilenameUtils.concat(basePath, outputFolder);
+
+    public static void checkDirExists(File path){
+        if (!path.exists()) {
+            try {
+                path.mkdir();
+                return;
+            } catch (SecurityException se) {
+                se.printStackTrace();
+            }
+        }
+    }
+
+    // flip images
+    public static void flipFiles(ImageTransformUtils imgTransform, int rotationAngle){
+        imgTransform.flipImage(rotationAngle); // 180 and other values
+    }
+
+    // center and resize images
+    public void initCenterResize(ImageTransformUtils imgTransform, int newW, int newH){
+        imgTransform.centerResize(newW, newH);
+    }
+
+    // change pixels...
+
+    // change file contents
+    public void basicInit(File inputPath) {
+        boolean recursive = true;
+
+        ImageTransformUtils imgTransform = new ImageTransformUtils();
+
+        if(inputPath.isDirectory()) {
+            Iterator iter = FileUtils.iterateFiles(inputPath, ImageNetLoader.ALLOWED_FORMATS, recursive);
+
+            int numSaved = 0;
+            int numDel = 0;
+            while(iter.hasNext()) {
+                File fileName = (File) iter.next();
+                imgTransform.convertToBuffer(fileName);
+                // ******** add other transforms here ********
+                if(imgTransform.getImage()==null)
+                    numDel++;
+                else {
+                    imgTransform.saveImage(new File(outputPath+numSaved));
+                    numSaved++;
+                }
+            }
+            log.info("Number of files deleted: " + numDel);
+        } else {
+            imgTransform.convertToBuffer(inputPath);
+        }
+
+    }
+
+    // rename files
+    public void fixFileName(Path path) {
+        try {
+            Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path filePath, BasicFileAttributes attrs) throws IOException {
+                    Path fileName = filePath.getFileName();
+                    Path dirPath = filePath.getParent();
+                    String newFileName = fileName.toString().trim().toLowerCase().replace(" ", "_");
+                    Path newFilePath = new File(dirPath.toString(), newFileName).toPath();
+                    Files.move(filePath, newFilePath);
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    // track size of files and group them into 128MB size
+    public static void segmentFileSplit(File inputPath, PreProcessData ppd){
+        long length = 0;
+        long limitSize = 128 * 1000000;
+        Iterator<File> subFiles = FileUtils.iterateFiles(inputPath, ImageNetLoader.ALLOWED_FORMATS, true);
+        String storePath = FilenameUtils.concat(TEMP_DIR, "storePath");
+        File storeFiles = new File(storePath);
+        checkDirExists(storeFiles);
+
+        // TODO add below to FileSplit
+        while(subFiles.hasNext()) {
+            int count = 0;
+            while (length < limitSize) {
+                if (subFiles.hasNext()) {
+                    File f = subFiles.next();
+                    try {
+                        Files.copy(f.toPath(), new File(storePath, f.getName()).toPath(), StandardCopyOption.REPLACE_EXISTING);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    length += f.length();
+                } else
+                    break;
+            }
+
+            // TODO process images before grouping into sequence files
+            ppd.setupSequnceFile(storePath + "/*", outputPath + "/" + count);
+            ppd.checkFile(outputPath + "/" + count);
+            count++;
+        }
+        try {
+            FileUtils.deleteDirectory(storeFiles);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // TODO diversify what files are pulled out by walking path?
+//        Path path = Paths.get(inputPath.toString());
+//        FileVisitor<Path> fv = new SimpleFileVisitor<Path>() {
+//            @Override
+//            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+//                    throws IOException {
+//                ;
+//                return FileVisitResult.CONTINUE;
+//            }
+//        };
+//
+//        try {
+//            Files.walkFileTree(path, fv);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+    }
+
+    public static void main(String[] args){
+        boolean save = true;
+        // TODO change what is called - group images & make diverse, convert images, save as sequence
+        PreProcessData ppd = new PreProcessData(save);
+        checkDirExists(new File(inputPath));
+        checkDirExists(new File(outputPath));
+        segmentFileSplit(new File(inputPath), ppd);
+
+    }
+
+}
---
+++ src/main/java/imagenet/Utils/PreProcessData.java
@@ -0,0 +1,124 @@
+package imagenet.Utils;
+
+;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import org.apache.spark.input.PortableDataStream;
+import org.canova.api.writable.Writable;
+import org.canova.image.recordreader.ImageNetRecordReader;
+import org.canova.spark.functions.data.FilesAsBytesFunction;
+import org.canova.spark.functions.data.RecordReaderBytesFunction;
+
+import org.deeplearning4j.spark.canova.CanovaDataSetFunction;
+import org.nd4j.linalg.dataset.DataSet;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.regex.Pattern;
+
+/**
+ * Prep data as sequence files to group small files into larger batches to enable
+ * optimized processing with Spark and Hadoop.
+ *
+ */
+public class PreProcessData {
+
+    protected static final String TEMP_DIR = FilenameUtils.concat(System.getProperty("java.io.tmpdir"), "ImageNetSeqFiles");
+    protected static String SEQUENCE_DIR;
+    protected JavaPairRDD<Text, BytesWritable> sequenceFile;
+    protected boolean save = false;
+    protected JavaSparkContext sc;
+
+    public PreProcessData(boolean save){
+        this(null, save);
+    }
+
+    public PreProcessData(JavaSparkContext sc, boolean save){
+        this.save = save;
+        this.sc = sc != null? sc : setupLocalSpark();
+    }
+
+    private JavaSparkContext setupLocalSpark(){
+        // Spark configuration
+        SparkConf sparkConf = new SparkConf();
+        sparkConf.setAppName("DataSequence");
+        sparkConf.setMaster("local[*]");
+        return new JavaSparkContext(sparkConf);
+    }
+
+    private void saveFiles(String outputPath){
+        SEQUENCE_DIR = outputPath != null? outputPath: FilenameUtils.concat(TEMP_DIR, "tmp");
+        if(new File(outputPath).exists()){
+            try {
+                FileUtils.deleteDirectory(new File(outputPath));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        sequenceFile.saveAsNewAPIHadoopFile(SEQUENCE_DIR, Text.class, BytesWritable.class, SequenceFileOutputFormat.class);
+        System.out.println("\n****************Sequence files saved at " + outputPath.toString() + " ********************");
+    }
+
+    public void setupSequnceFile(String inputPath, String outputPath){
+        // pass in file path with *.[file type] (e.g. jpg) to limit type of files loaded or use main dir and ensure only image files in subDir
+
+        JavaPairRDD<String, PortableDataStream> stream = sc.binaryFiles(inputPath);
+        sequenceFile = stream.mapToPair(new FilesAsBytesFunction());
+        sequenceFile.cache();
+        System.out.println("\n************Sequence file loaded************************");
+        // Save SequenceFiles:
+        if(save) {
+            saveFiles(outputPath);
+            sequenceFile.unpersist();
+        }
+    }
+
+    // Doesn't work...
+//    public void setupSequnceFile(List<File> files, String outputPath){
+//        JavaRDD<File> stream = sc.parallelize(files);
+//        sequenceFile = stream.mapToPair(new PairFunction<File, Text, BytesWritable>() {
+//            @Override
+//            public Tuple2<Text, BytesWritable> call(File file) throws Exception {
+//                byte[] bytes = Files.toByteArray(file);
+//                return new Tuple2<>(new Text(file.toString()), new BytesWritable(bytes));
+//            }
+//        });
+//        sequenceFile.cache();
+//        System.out.println("\n************Sequence file loaded************************");
+//        // Save SequenceFiles:
+//        if(save) {
+//            saveFiles(outputPath);
+//        }
+//    }
+
+    // TODO remove - temp to test results
+    public void checkFile(String inputPath){
+        JavaPairRDD<Text, BytesWritable> data = sc.sequenceFile(inputPath, Text.class, BytesWritable.class);
+        RecordReaderBytesFunction recordReaderFunc = new RecordReaderBytesFunction(
+                new ImageNetRecordReader(40, 40, 3, FilenameUtils.concat(ImageNetLoader.BASE_DIR, ImageNetLoader.LABEL_FILENAME), true, Pattern.quote("_")));
+        JavaRDD<Collection<Writable>> rdd = data.map(recordReaderFunc);
+        JavaRDD<DataSet> ds = rdd.map(new CanovaDataSetFunction(-1, 1860, false));
+        List<DataSet> v = ds.take(1);
+    }
+
+    public JavaPairRDD<Text, BytesWritable> getFile(){
+        return sequenceFile;
+    }
+
+    public void unpersist(){
+        sequenceFile.unpersist();
+    }
+
+}
